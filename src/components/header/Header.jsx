0179e28c1dded3979ea6d1bef3c9e0263f53eb6a
pothikit <tytanhso71.@gmail.com>
2023-08-08 14:30:20 +0600

    icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import logo from "../../hostflu-logo.png"
 import "./header.css"
-import { FaPhoneVolume, FaEnvelope } from "react-icons/fa";
+import { FaPhoneVolume, FaEnvelope, FaWhatsapp } from "react-icons/fa";
 import { RxAvatar } from "react-icons/rx";
 import { Link } from 'react-router-dom';
 
@@ -11,7 +11,7 @@ export const Header = () => {
             <div className="top-header py-2">
                 <div className="container mx-auto">
                     <ul className='flex justify-end items-center gap-12 text-white text-lg'>
-                        <li className=''><a href="https://wa.me/+8801719375526" className='flex items-center gap-4'><FaPhoneVolume />+880 1719-375526</a></li>
+                        <li className=''><a href="https://wa.me/+8801719375526" className='flex items-center gap-4'><span className="rotate-[270deg]"><FaWhatsapp /></span>+880 1719-375526</a></li>
                         <li className=''><a href="mailto:hostflu@gmail.com" className='flex items-center gap-4'><FaEnvelope />hostflu@gmail.com</a></li>
                         <li className=''><Link className="flex items-center gap-4" to="login"><RxAvatar />login</Link></li>
                         <li className=''><Link className="flex items-center gap-4" to="login"><RxAvatar />Registration</Link></li>
