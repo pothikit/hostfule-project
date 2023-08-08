80e0a02b06a0f706ce7cd3c6ad99386a3af3c49f
pothikit <tytanhso71.@gmail.com>
2023-08-08 14:49:39 +0600

    navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,10 @@
 import * as React from "react";
 import logo from "../../hostflu-logo.png"
 import "./header.css"
-import { FaPhoneVolume, FaEnvelope, FaWhatsapp } from "react-icons/fa";
+import { FaEnvelope, FaWhatsappSquare } from "react-icons/fa";
 import { RxAvatar } from "react-icons/rx";
 import { Link } from 'react-router-dom';
+import { Navbar } from "./Navbar";
 
 export const Header = () => {
     return (
@@ -11,17 +12,20 @@ export const Header = () => {
             <div className="top-header py-2">
                 <div className="container mx-auto">
                     <ul className='flex justify-end items-center gap-12 text-white text-lg'>
-                        <li className=''><a href="https://wa.me/+8801719375526" className='flex items-center gap-4'><span className="rotate-[270deg]"><FaWhatsapp /></span>+880 1719-375526</a></li>
+                        <li className=''><a href="https://wa.me/+8801719375526" className='flex items-center gap-4'><span className="rotate-[270deg]"><FaWhatsappSquare /></span>+880 1719-375526</a></li>
                         <li className=''><a href="mailto:hostflu@gmail.com" className='flex items-center gap-4'><FaEnvelope />hostflu@gmail.com</a></li>
                         <li className=''><Link className="flex items-center gap-4" to="login"><RxAvatar />login</Link></li>
                         <li className=''><Link className="flex items-center gap-4" to="login"><RxAvatar />Registration</Link></li>
                     </ul>
                 </div>
             </div>
-            <nav className=''>
+            <nav className='border-b border-primary'>
                 <div className="container mx-auto">
-                    <div className="logo">
-                        <img src={logo} alt="Logo" />
+                    <div className="navigation-menu flex justify-between items-center py-2">
+                        <div className="logo">
+                            <img src={logo} alt="Logo" />
+                        </div>
+                        <Navbar></Navbar>
                     </div>
                 </div>
             </nav>
