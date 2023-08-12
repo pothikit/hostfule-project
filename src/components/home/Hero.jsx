e22f9fea66aea39afefd94d0887ad0c34ac836d6
pothikit <tytanhso71.@gmail.com>
2023-08-12 13:33:47 +0600

    hero done

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import heroImg from "../../asstes/shutterstock.png"
+import { BsFillCheckSquareFill } from 'react-icons/bs'
 function Hero() {
     return (
         <section className='py-16'>
@@ -16,15 +17,28 @@ function Hero() {
                                 unmatched Guru Crew Support, we provide everything you
                                 need to create a successful website.</p>
                             {/* list item hero */}
-                            <ul>
-                                <li></li>
+                            <ul className='space-y-5'>
+                                <li className='flex gap-5'>
+                                    <span className='text-2xl'><BsFillCheckSquareFill className='text-primary' /></span>
+                                    <span>Cloud Backup</span>
+                                </li>
+                                <li className='flex gap-5'>
+                                    <span className='text-2xl'><BsFillCheckSquareFill className='text-primary' /></span>
+                                    <span>Server Security</span>
+                                </li>
+                                <li className='flex gap-5'>
+                                    <span className='text-2xl'><BsFillCheckSquareFill className='text-primary' /></span>
+                                    <span>Affordable Price</span>
+                                </li>
                             </ul>
+                            <div className="buttons mt-10 space-x-5">
+                                <button className='hover:bg-gradient-to-r from-primary to-secondary py-3 px-5 rounded-lg border border-primary duration-300 hover:text-white'>Get Serrated</button>
+                                <button className='hover:bg-gradient-to-r from-primary to-secondary py-3 px-5 rounded-lg border border-primary duration-300 hover:text-white'>Help Me Choose</button>
+                            </div>
                         </div>
                     </div>
-                    <div className=''>
-                        <div>
-                            <img src={heroImg} alt="" />
-                        </div>
+                    <div>
+                        <img src={heroImg} alt="" />
                     </div>
                 </div>
             </div>
