1d7fbdb639c5c9a8d9bd13605c814965ba96ce79
pothikit <tytanhso71.@gmail.com>
2023-08-13 14:51:14 +0600

    three card end

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import DomainSeach from './domainSeach'
+import Threcard from './Threcard'
 
 function Home() {
     return (
         <main>
             <Hero></Hero>
             <DomainSeach></DomainSeach>
+            <Threcard></Threcard>
         </main>
     )
 }
