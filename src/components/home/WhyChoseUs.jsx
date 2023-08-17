90bf71b8900e70c5e8532efee9a95e9db87f1a37
pothikit <tytanhso71.@gmail.com>
2023-08-17 14:22:05 +0600

    service box designed

--- src/components/home/WhyChoseUs.jsx
+++ src/components/home/WhyChoseUs.jsx
@@ -9,9 +9,9 @@ function WhyChoseUs() {
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-32'>
                     {
                         ourService.map((data) => (
-                            <div className='p-10 bg-gray-200 rounded-lg' key={data.id}>
-                                <img src={`${data.icon}`} alt={data.title} />
-                                <h2>{data.title}</h2>
+                            <div className='py-10 px-20 text-center serviceBox rounded-lg' key={data.id}>
+                                <img src={`${data.icon}`} className='mx-auto' alt={data.title} />
+                                <h2 className='text-2xl my-4 font-semibold'>{data.title}</h2>
                                 <p>{data.description}</p>
                             </div>
                         ))
