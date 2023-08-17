eac81c37d2c57662109f596395003c159282e5db
pothikit <tytanhso71.@gmail.com>
2023-08-17 14:15:45 +0600

    service img showing from public

--- src/components/home/WhyChoseUs.jsx
+++ src/components/home/WhyChoseUs.jsx
@@ -9,7 +9,7 @@ function WhyChoseUs() {
                 <div className='grid grid-cols-1 md:grid-cols-3 gap-32'>
                     {
                         ourService.map((data) => (
-                            <div className='p-10 rounded-xl bg-gray-200 rounded-lg' key={data.id}>
+                            <div className='p-10 bg-gray-200 rounded-lg' key={data.id}>
                                 <img src={`${data.icon}`} alt={data.title} />
                                 <h2>{data.title}</h2>
                                 <p>{data.description}</p>
