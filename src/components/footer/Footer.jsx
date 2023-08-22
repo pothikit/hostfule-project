c38d7df1fadce0ea91d39d4d5cc7f117a2b6db47
pothikit <tytanhso71.@gmail.com>
2023-08-22 17:06:42 +0600

    footer complete without payment

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import logo from "../../asstes/footer-logo.png"
 import "./footer.css"
+import { MdNetworkWifi } from "react-icons/md"
+import { FaFacebookF, } from "react-icons/fa"
+import { AiFillInstagram, AiFillYoutube } from "react-icons/ai"
+import { TbWorld } from "react-icons/tb"
 
 function Footer() {
     return (
@@ -60,8 +64,48 @@ function Footer() {
                     </div>
                     {/* col main div end */}
                 </div>
+                <div className="footer-bottom py-20">
+                    <div className="grid gird-cols-1 grid-cols-3 gap-20">
+                        <div className="bg-gray-700 opacity-20 payment-gatway">
+                            payment
+                        </div>
+                        <div className="newsletter">
+                            <h2 className='text-white text-xl font-semibold'>Subscribe Our Newsletter</h2>
+                            <p className='text-white leading-7 w-[70%] my-5'>By subscribing with your mail, you will accept our privacy policy.</p>
+                            <form action="" className='flex bg-white p-2 justify-between items-center rounded-lg'>
+                                <input required type="text" className='flex-1 text-gray-400 outline-none p-2' placeholder='Enter Your Email Address' />
+                                <input type="submit" value="SUBSCRIBE" className='px-4 py-2 bg-gradient-to-r from-primary to-secondary hover:bg-primary ml-4 rounded-lg text-white cursor-pointer' />
+                            </form>
+                        </div>
+                        <div className="social space-y-5">
+                            <div className='s-title text-white flex items-center gap-3'>
+                                <span className='text-xl rotate-45'><MdNetworkWifi /></span>
+                                <h3>Follow Our Social Media</h3>
+                            </div>
+                            <ul className='flex items-center gap-5'>
+                                <li className='text-3xl text-primary rounded-xl bg-white p-4 mr-2'><FaFacebookF /></li>
+                                <li className='text-3xl text-primary rounded-xl bg-white p-4 mr-2'><AiFillInstagram /></li>
+                                <li className='text-3xl text-primary rounded-xl bg-white p-4 mr-2'><TbWorld /></li>
+                                <li className='text-3xl text-primary rounded-xl bg-white p-4 mr-2'><AiFillYoutube /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
             </div>
             {/* container */}
+            <div className="copyright py-4 text-white bg-primary">
+                <div className="container mx-auto">
+                    <flex className="flex items-center justify-between">
+                        <span>Copyright © HostFlu 2023</span>
+                        <ul className='flex items-center'>
+                            <li className='border-r border-gray-300 px-3'>Terms of Service</li>
+                            <li className='px-4'>Privacy Policy</li>
+                        </ul>
+                    </flex>
+
+                </div>
+            </div>
         </footer>
     )
 }
