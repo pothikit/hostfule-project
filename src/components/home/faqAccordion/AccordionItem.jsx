89ffff47828be0ba69b0ed49c4313fa48f5a67b2
pothikit <tytanhso71.@gmail.com>
2023-08-23 17:35:16 +0600

    faq arrow add and working fine

--- src/components/home/faqAccordion/AccordionItem.jsx
+++ src/components/home/faqAccordion/AccordionItem.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { Collapse } from 'react-collapse'
+import { RiArrowUpSLine } from "react-icons/ri"
 
 function AccordionItem({ open, toggle, title, desc }) {
     return (
         <div className='py-3'>
-            <div onClick={toggle} className='cursor-pointer'>
-                <h2 className='text-xl font-medium pb-3 border-b border-gray-400 cursor-pointer'>{title}</h2>
+            <div onClick={toggle} className='cursor-pointer pb-3 border-b mb-4 border-gray-400 flex items-center w-full justify-between'>
+                <h2 className='text-xl font-medium cursor-pointer'>{title}</h2>
+                <span className={`text-2xl duration-500 ${open ? "rotate-0" : "rotate-180"}`}><RiArrowUpSLine /></span>
             </div>
-            <Collapse isOpened={open} className={`mt-3 p-1`}>{desc}</Collapse>
+            <Collapse isOpened={open}>{desc}</Collapse>
         </div>
     )
 }
