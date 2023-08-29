b667e3051f680b16902c47c68a28ecf8aef4c4c3
pothikit <tytanhso71.@gmail.com>
2023-08-29 13:13:10 +0600

    shared hosting

--- src/components/sharedHosting/IndividualServices.jsx
+++ src/components/sharedHosting/IndividualServices.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
-function IndividualServices() {
+function IndividualServices(props) {
+    const { image, title, desc } = props;
     return (
-        <div>IndividualServices</div>
+        <div className='max-w-lg mx-auto'>
+            <img src={image} className='w-52' alt="title" />
+            <h2 className='text-2xl md:4xl my-5'>{title}</h2>
+            <p className='text-base md:text-xl text-justify'>{desc}</p>
+        </div>
     )
 }
 
--- src/components/sharedHosting/SharedHosting.jsx
+++ src/components/sharedHosting/SharedHosting.jsx
@@ -76,9 +76,19 @@ function SharedHosting() {
                     </div>
                 </div>
             </section>
-            <section>
+            <section className='py-32'>
                 <div className="container mx-auto px-2">
-                    <IndividualServices></IndividualServices>
+                    <div className='grid grid-cols lg:grid-cols-2 gap-5 lg:gap-10'>
+                        <IndividualServices image={cpanel} title="C Panel" desc="cPanel is a powerful and versatile web hosting control panel that provides users with a wide range of tools and features to manage their websites. With its user-friendly interface, robust security measures, and extensive customization options, cPanel has become the go-to choice for web developers"></IndividualServices>
+
+                        <IndividualServices image={linux} title="Cloud Linux" desc="CloudLinux is a lightweight operating system based on the Linux kernel and targeted at servers, with focus on scalability, security.It provides a stable and secure platform for running applications in the cloud. CloudLinux is a popular choice for cloud providers, including Amazon Web Services, Microsoft Azure, and Google Cloud Platform."></IndividualServices>
+
+                        <IndividualServices image={ssl} title="Free SSL" desc="SSL stands for secure Socket Layer. It is a protocol that allows encrypted communication over a public network. SSL is also used to authenticate websites and users, and protects data from unauthorized access. It is widely used to secure online transactions and other sensitive information."></IndividualServices>
+
+                        <IndividualServices image={wordpresskit} title="WordPresskit" desc="WordPresskit is a company that offers plugins for website creation using WordPress. Their products are designed to make it easier to manage your WordPress website, like installing themes and plugins. They also offer tutorials and support to help you along the way."></IndividualServices>
+
+                        <IndividualServices image={freeMigration} title="Free Migration" desc="We offer a free website migration service, which allows you to transfer your website from another hosting provider to HostFlu. We offer this migration service to ensure that your website is up and running smoothly on HostFlu."></IndividualServices>
+                    </div>
                 </div>
             </section>
         </main>
