1fc58ff9e08fefee9e866cc188587e2e33f8e712
pothikit <tytanhso71.@gmail.com>
2023-11-12 17:09:58 +0600

    gap reduced

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -25,7 +25,7 @@ function Footer() {
         <>
             <div className="container mx-auto -mt-20">
                 <div className='bg-white z-10 relative bottom-[-120px] p-6 rounded-2xl border border-[#cccccc61]'>
-                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-20 items-center">
+                    <div className="grid grid-cols-1 lg:grid-cols-3 gap-10 lg:gap-10 items-center">
                         <div className='flex items-center justify-center'>
                             {/* <img src={supportImg} alt="Our Support" className='w-full mx-auto' /> */}
                             <Player
