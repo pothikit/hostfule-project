4fcb97628bf06e197636c587c36579fb168c51fa
pothikit <tytanhso71.@gmail.com>
2023-12-07 20:32:31 +0600

    table and data update and so on

--- src/components/dedicatedServer/ServerLocation.jsx
+++ src/components/dedicatedServer/ServerLocation.jsx
@@ -12,7 +12,7 @@ const ServerLocation = ({ serverLocation }) => {
                         <div className='shadow-lg border rounded-lg locationCard px-6 sm:py-2 relative bg-slate-50'>
                             <div className='pt-32'>
                                 <img src={countryFlag} alt="" className='w-16 md:w-14' />
-                                <p className='text-base text-primary font-bold capitalize pt-5 -pb-5'>{countryName}</p>
+                                <p className='text-base text-slate-600 font-bold capitalize pt-5 -pb-5'>{countryName}</p>
                                 <h3 key={idx} className='md:text-xl font-semibold capitalize'>{items.name}</h3>
                             </div>
                             <div className='absolute top-0 right-0 max-w-[75%] bg-red-100 h-full z-10 rounded-bl-full'>
--- src/components/dedicatedServer/locationDatatable/LocationDatatabel.jsx
+++ src/components/dedicatedServer/locationDatatable/LocationDatatabel.jsx
@@ -6,6 +6,7 @@ import { useParams } from 'react-router-dom';
 const LocationDatatabel = () => {
     const [locationData, setLocationData] = useState([])
     const { location } = useParams();
+    const city = location.split("-")[0];
     const country = location.split("-")[1];
 
     useEffect(() => {
@@ -15,61 +16,67 @@ const LocationDatatabel = () => {
                 setLocationData(data.countries)
             })
     }, [])
-    const countryWise = locationData.filter(items => items.countryName === country)
-    console.log(countryWise)
+    const specificCountry = locationData?.find(countries => countries.countryName === country)
+    const allCities = specificCountry?.city?.filter(cities => cities.name === city)
+    // console.log(allCities)
     return (
         <div className='py-20 container mx-auto'>
             <div>
-                <div>
-                    <ul className='flex justify-between gap-2 bg-primary py-2 px-3 rounded-md text-white font-semibold'>
-                        <li className='uppercase'>CPU</li>
-                        <li className='uppercase'>RAM</li>
-                        <li className='uppercase'>STORAGE</li>
-                        <li className='uppercase'>NETWORK</li>
-                        <li className='uppercase'>PRICE</li>
-                        <li className='uppercase'>Availability</li>
-                    </ul>
+                <div className=' bg-orange-500 hidden xl:block rounded-md px-3'>
+                    <div className='flex items-center'>
+                        <div className='uppercase whitespace-nowrap text-white font-semibold xl:pl-3 xl:max-w-[150px]'>Server Id</div>
+                        <ul className='xl:grid grid-cols-3 md:grid-cols-6 gap-6 py-5 text-white font-semibold w-full'>
+                            <li className='uppercase ml-20'>CPU</li>
+                            <li className='uppercase text-center'>RAM</li>
+                            <li className='uppercase'>STORAGE</li>
+                            <li className='uppercase'>NETWORK</li>
+                            <li className='uppercase'>PRICE</li>
+                            <li className='uppercase'>Availability</li>
+                        </ul>
+                    </div>
                 </div>
-                {
-                    countryWise.map((items) => {
-                        return items.city.map((items) => (
-                            items.details.map((details, idx) => (
-                                (
-                                    <div key={idx} className='flex items-center justify-between gap-2 border mt-2 shadow-lg py-6 px-3 rounded-md font-semibold'>
-                                        <div className='flex items-center gap-3'>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-3 xl:grid-cols-1'>
+                    {
+                        allCities?.map((cities) => (
+                            cities?.details?.map((details, idx) => (
+                                <div key={idx} className='shadow-lg border flex xl:px-6 mt-3 rounded-md flex-col xl:flex-row p-4 xl:p-0 flex-start xl:items-center items-start'>
+                                    <div className='whitespace-nowrap ml-5 xl:ml-0 py-3 font-bold xl:max-w-[150px]'><span className='inline xl:hidden'>Server Id :</span> {details.id}</div>
+                                    <div className='grid grid-cols-1 xl:grid-cols-6 gap-3 items-center xl:py-4 xl:p-0 font-semibold w-full'>
+                                        <div className='flex items-start flex-row-reverse xl:flex-row justify-end xl:justify-start text-start gap-3 ml-5 xl:ml-0 xl:pl-6'>
                                             <img src={details.brand} alt="" className='w-14' />
                                             <div className='flex flex-col'>
-                                                <span className='font-thin'>{details.cpu}</span>
+                                                <span className='font-bold xl:font-thin text-lg xl:text-base'>{details.cpu}</span>
                                                 <span className='font-thin text-xs'>{details?.ghz}</span>
                                             </div>
                                         </div>
-
-                                        <div className='flex flex-col font-thin'>
-                                            <span>{details.ram}</span>
+                                        <div className='flex gap-2 xl:flex-col xl:text-center font-thin ml-5 xl:ml-0 xl:pl-6'>
+                                            <span className='font-bold xl:font-thin text-lg xl:text-base'>{details.ram}</span>
                                             <span>{details.ddr}</span>
                                         </div>
-                                        <div className='flex flex-col font-thin'>
-                                            <span>{details.storage}</span>
+                                        <div className='flex xl:flex-col font-thin ml-5 xl:ml-0 xl:pl-6'>
+                                            <span className='font-bold xl:font-thin text-lg xl:text-base'>{details.storage}</span>
                                             <span>{details?.alternativeStorage}</span>
                                         </div>
-                                        <div className='flex flex-col font-thin'>
-                                            <span>{details?.networkSpeed}</span>
+                                        <div className='flex gap-2 xl:flex-col font-thin ml-5 xl:ml-0 xl:pl-6'>
+                                            <span className='font-bold xl:font-thin text-lg xl:text-base'>{details?.networkSpeed}</span>
                                             <span>{details?.maximumSpeed}</span>
                                             <span className='italic'>{details?.fairUse}</span>
                                         </div>
-                                        <div>
+                                        <div className='ml-5 xl:ml-0 xl:pl-6'>
                                             <h2><del>{details?.oldPrice}</del></h2>
-                                            <h2 className='flex'>${details?.newPrice}</h2>
+                                            <h2 className='flex font-bold xl:font-thin text-lg xl:text-base'>${details?.newPrice}</h2>
                                         </div>
                                         <div>
-                                            <button className='bg-[#6c757d] py-2 px-4 rounded-md text-white'>Check Now</button>
+                                            <a target='_blank' rel="noreferrer" href="https://m.me/198137493375467">
+                                                <button className='bg-[#6c757d] hover:bg-slate-600 duration-300 py-2 px-4 rounded-md text-white w-full xl:w-auto'>Check Now</button>
+                                            </a>
                                         </div>
                                     </div>
-                                )
+                                </div>
                             ))
                         ))
-                    })
-                }
+                    }
+                </div>
             </div>
         </div>
     );
