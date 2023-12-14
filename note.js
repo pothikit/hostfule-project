3b85c056fe61ea1e59bbafbe4cf6533bc2f5b1c5
pothikit <tytanhso71.@gmail.com>
2023-12-14 18:07:11 +0600

    country id add

---
+++ note.js
@@ -0,0 +1,7 @@
+// location country card border radius
+// amd brand img center in list page
+// FH - id remove bold
+//
+//city name remove and a little bit dicrease font size
+// specify a google font
+// country name font size a little decrease
