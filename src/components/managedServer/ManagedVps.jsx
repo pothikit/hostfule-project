75e21ac8e33f9c508c6f0a9f39b1b325d988edec
pothikit <tytanhso71.@gmail.com>
2023-12-21 17:24:56 +0600

    range slider with priceing

--- src/components/managedServer/ManagedVps.jsx
+++ src/components/managedServer/ManagedVps.jsx
@@ -20,18 +20,16 @@ const ManagedServer = () => {
     }, [])
 
     const rangeData = data.find((items) => items.id === rangevalue)
-
-    console.log(rangeData)
     return (
         <main>
             <PageBanner animationfile={managedvpsAnimation} pageTitle={["Hassle-Free ", <span className='text-primary' key={1}>M</span>, "anaged VPS Hosting"]} animationName=""></PageBanner>
-
             <section className='py-20 custom-slider'>
                 <div className="container mx-auto">
-                    <div className='max-w-[1000px] mx-auto bg-white border rounded-md' style={{ boxShadow: "5px 5px 20px #ccc" }}>
+                    <div className='max-w-[1000px] mx-auto bg-white border rounded-md' style={{ boxShadow: "5px 5px 30px 5px #ddd" }}>
                         <div className='p-3'>
-                            <div className='bg-white'>
-                                <Slider min={1} max={6} step={1} value={rangevalue} onChange={handleChange} className='cursor-pointer' />
+                            <div className='bg-white p-6'>
+                                <Slider min={1} max={6} step={1} duration={2000} value={rangevalue} onChange={handleChange} className='cursor-pointer' />
+
                                 <div className='flex justify-between items-center font-bold'>
                                     <h2>Storage 1GB</h2>
                                     <h2>Storage 2GB</h2>
@@ -42,23 +40,41 @@ const ManagedServer = () => {
                                 </div>
                             </div>
                         </div>
-                        <div className='border mt-7 p-2 py-10 bg-slate-50 grid grid-cols-1 lg:grid-cols-3 gap-7'>
+                        <div className='border mt-7 p-6 bg-slate-100 grid grid-cols-1 lg:grid-cols-3 gap-7'>
                             <div className='grid grid-cols-1 md:grid-cols-2 gap-4 lg:col-span-2 p-3'>
-                                <div className='border rounded-md shadow-lg flex items-center justify-between bg-slate-300 py-5 px-3'><span>Core</span>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>Core</span>
                                     <h2>{rangeData?.cpu_core}</h2>
                                 </div>
-                                <div className='border rounded-md shadow-lg flex items-center justify-between bg-slate-300 py-5 px-3'><span>Ram</span>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>Ram</span>
                                     <h2>{rangeData?.ram}</h2>
                                 </div>
-                                <div className='border rounded-md shadow-lg flex items-center justify-between bg-slate-300 py-5 px-3'><span>Storage</span>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>Storage</span>
                                     <h2>{rangeData?.ssd_space}</h2>
                                 </div>
-                                <div className='border rounded-md shadow-lg flex items-center justify-between bg-slate-300 py-5 px-3'><span>Bandwidth</span>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>Bandwidth</span>
                                     <h2>{rangeData?.bandwidth}</h2>
+                                </div>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>thread</span>
+                                    <h2>{rangeData?.thread}</h2>
+                                </div>
+                                <div className='border rounded-md shadow flex items-center justify-between bg-white py-6 px-3'><span>IP</span>
+                                    <h2>{rangeData?.ip_address}</h2>
+                                </div>
+                            </div>
+                            <div>
+                                <div className='p-4 border rounded-md shadow-md'>
+                                    <h3 className='border-b pb-4 mb-4'>Managed</h3>
+                                    <div className='border-b pb-4 md-4 text-center'>
+                                        <h1 className='text-xl md:text-2xl lg:text-4xl font-bold'><span className=''>Price</span> <span>{rangeData?.price}</span></h1>
+                                        <button className='bg-primary text-white px-3 py-1 md:py-2 text-xl md:px-10 mt-5 rounded-md'>Order Now</button>
+                                    </div>
+                                    <div className='mt-3'>
+                                        <h3 className='font-medium'>Available Location</h3>
+                                        <h3 className='text-sm'>(Slelct During checkout)</h3>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div><span>Price</span><span>{rangeData.price}</span></div>
                     </div>
 
                 </div>
