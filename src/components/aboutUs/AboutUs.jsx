c9438e6689d47db078138ef4e677393c12e9070e
pothikit <tytanhso71.@gmail.com>
2024-01-03 19:49:45 +0600

    team section

--- src/components/aboutUs/AboutUs.jsx
+++ src/components/aboutUs/AboutUs.jsx
@@ -5,19 +5,28 @@ import webserver from "../../asstes/webserver.jpg"
 import Testimonial from './Testimonila'
 import Swal from 'sweetalert2'
 import "./about.css"
+import { FaFacebook, FaGithub, FaLinkedin, FaWhatsapp } from "react-icons/fa";
+
 
 function AboutUs() {
     const [payment, setPayment] = useState([])
+    const [allteam, setAllTeam] = useState([])
     useEffect(() => {
         fetch("paymentData.json")
             .then(res => res.json())
             .then(data => setPayment(data.payment))
     }, [])
+    // fetching team data
+    useEffect(() => {
+        fetch("paymentData.json")
+            .then(res => res.json())
+            .then(data => setAllTeam(data.team))
+    }, [])
     const handlePayment = (items) => {
         Swal.fire({
             html: `
            <div>
-                <div class="qrCode flex justify-evenly flex-col sm:flex-row w-full p-2">
+                <div class="qrCode flex justify-between flex-col sm:flex-row w-full p-2">
                     <div class=""><img src='${items.qrCode}' class=""/></div>
                     <div class="flex justify-start flex-col items-start gap-4">
                          <h3 class="font-bold mt-5">Account No: </h3><input value=${items?.account_number} readonly class="border p-1 outline-0 text-xl"/>
@@ -26,12 +35,12 @@ function AboutUs() {
                  <div>
                     <h3 class="text-left font-bold text-2xl">Rules:</h3>
                     <ul class="flex flex-col gap-3 items-start font-semibold mt-3">
-                      <li>${items?.desc1}</li>
-                      <li>${items?.desc2}</li>
-                      <li>${items?.desc3}</li>
-                      <li>${items?.desc4}</li>
-                      <li>${items?.desc5}</li>
-                      <li>${items?.desc6}</li>
+                      <li class="text-left">${items?.desc1}</li>
+                      <li class="text-left">${items?.desc2}</li>
+                      <li class="text-left">${items?.desc3}</li>
+                      <li class="text-left">${items?.desc4}</li>
+                      <li class="text-left">${items?.desc5}</li>
+                      <li class="text-left">${items?.desc6}</li>
                     </ul>
                  </div>
             </div>
@@ -94,6 +103,7 @@ function AboutUs() {
                                 <span>Privacy Protection: </span>Understanding the importance of privacy, we offer privacy protection services to safeguard our clients' personal information.
                             </p>
 
+
                             <p className='text-slate-600 md:text-lg'>
                                 <span>SSL Certificate: </span>We offer SSL certificates to safeguard sensitive information and facilitate secure transactions. </p>
                             <p className='text-slate-600 md:text-lg'>
@@ -127,15 +137,40 @@ function AboutUs() {
             </section>
             <section className='py-20 px-1'>
                 <div className="container mx-auto">
+                    <h1 className="text-center text-slate-700 text-2xl md:text-[40px] mb-20 font-semibold">Our Team</h1>
+                    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-8 md:grid-cols-4 md:gap-10 overflow-hidden'>
+                        {
+                            allteam?.map((items, idx) => (
+                                <div key={idx} className='py-3 bg-slate-400 rounded-md relative group overflow-hidden'>
+                                    <img src={items?.image} alt={items?.name} />
+                                    <div className='absolute -bottom-full group-hover:bottom-0 duration-300 px-2 text-white h-full w-full bg-black flex flex-col justify-end bg-opacity-50 rounded-md'>
+                                        <ul className='px-2 flex justify-evenly gap-2 file mb-10 flex-wrap'>
+                                            <li className='hover:rotate-[360deg] duration-300 p-3'><a target='_blank' className='hover:text-blue-400 duration-200' rel='noreferrer' href={items?.fbUrl}>
+                                                <FaFacebook size={25} /></a>
+                                            </li>
+                                            <li className='hover:rotate-[360deg] duration-300 p-3'>
+                                                <a target='_blank' className='hover:text-blue-400 duration-200' rel='noreferrer' href={items?.linkedinUrl}>
+                                                    <FaLinkedin size={25} />
+                                                </a>
+                                            </li>
+                                            <li className='hover:rotate-[360deg] duration-300 p-3'>
+                                                <a target='_blank' className='hover:text-blue-400 duration-200' rel='noreferrer' href={`https://wa.me/${items?.whatsApp}`}><FaWhatsapp size={25} /></a>
+                                            </li>
+                                            <li className='hover:rotate-[360deg] duration-300 p-3'>
+                                                <a target='_blank' className='hover:text-blue-400 duration-200' rel='noreferrer' href={items?.gitHub}><FaGithub size={25} /></a>
+                                            </li>
+                                        </ul>
+                                        <div className='pb-5 text-center'>
+                                            <h3 className="text-2xl font-bold">{items.name}</h3>
+                                            <h4 className='text-primary font-semibold'>{items?.designation}</h4>
+                                            <h4 className='font-semibold'>{items?.organization}</h4>
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        }
 
-                    <h1 className="text-center text-slate-700 text-2xl md:text-[40px] mb-10 font-semibold">Our Team</h1>
-                    <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 sm:gap-3 md:grid-cols-4 md:gap-5'>
-                        <div className='py-3 bg-slate-400 rounded-md'>
-                            <img src="" alt="" />
-                        </div>
-                        <div className='py-3 bg-slate-400 rounded-md'></div>
-                        <div className='py-3 bg-slate-400 rounded-md'></div>
-                        <div className='py-3 bg-slate-400 rounded-md'></div>
+
                     </div>
                 </div>
             </section>
@@ -146,8 +181,8 @@ function AboutUs() {
                     <div className='payment-option grid grid-cols-2 md:grid-cols-4 gap-5 md:gap-10'>
                         {
                             payment?.map((items, idx) => (
-                                <button onClick={() => handlePayment(items)} key={idx} className="gatway bg-white flex border items-center justify-center p-2 sm:p-10 lg:px-10 cursor-pointer rounded-lg">
-                                    <img src={items?.bank_logo} alt={items?.bank_name} className='md:w-1/2' />
+                                <button onClick={() => handlePayment(items)} key={idx} className="gatway bg-white flex border items-center justify-center p-2 sm:p-5 cursor-pointer rounded-lg">
+                                    <img src={items?.bank_logo} alt={items?.bank_name} className='w-1/2' />
                                 </button>
                             ))
                         }
