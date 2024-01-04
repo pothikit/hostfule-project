1ea0e5f7968be3629074bf9576ed379ea4d0ffb5
pothikit <tytanhso71.@gmail.com>
2024-01-04 18:49:55 +0600

    terms and condition page

--- src/components/termsOfService/TermsOfService.jsx
+++ src/components/termsOfService/TermsOfService.jsx
@@ -4,7 +4,20 @@ import PageBanner from '../shared/pageBanner/PageBanner'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 function TermsOfService() {
-    const [tabNumber, setTabNumber] = useState("0")
+    const [tabNumber, setTabNumber] = useState(0);
+
+    const handleTab = (newTabNumber) => {
+        setTabNumber(newTabNumber);
+        console.log(newTabNumber)
+    };
+    const handleNext = () => {
+        const newTabNumber = (tabNumber + 1) % 2; // Assuming you have 2 tabs
+        setTabNumber(newTabNumber);
+    };
+    const handlePrev = () => {
+        const newTabNumber = (tabNumber - 1 + 2) % 2; // Assuming you have 2 tabs
+        setTabNumber(newTabNumber);
+    };
     return (
         <main>
             <section className=''>
@@ -14,28 +27,28 @@ function TermsOfService() {
             </section>
             <section className='py-20'>
                 <div className="container mx-auto">
-                    <Tabs tabIndex={tabNumber}>
-                        <TabList>
-                            <Tab>Title 1</Tab>
-                            <Tab>Title 2</Tab>
-                            <Tab>Title 3</Tab>
-                            <Tab>Title 4</Tab>
-                        </TabList>
+                    <div>
+                        <Tabs className="flex gap-10" selectedIndex={tabNumber} onSelect={handleTab}>
+                            <TabList className="flex flex-col w-2/12 gap-10 border-r">
+                                <Tab className="border-none cursor-pointer">Title 1</Tab>
+                                <Tab className="border-none cursor-pointer">Title 2</Tab>
+                            </TabList>
 
-                        <TabPanel>
-                            <h2>Any content 1</h2>
-                        </TabPanel>
-                        <TabPanel>
-                            <h2>Any content 2</h2>
-                        </TabPanel>
-                        <TabPanel>
-                            <h2>Any content 3</h2>
-                        </TabPanel>
-                        <TabPanel>
-                            <h2>Any content 4</h2>
-                        </TabPanel>
-                    </Tabs>
-                    <button onClick={() => setTabNumber(tabNumber + 1)}>next</button>
+                            <div>
+                                <TabPanel>
+                                    <h2>Any content 1</h2>
+                                </TabPanel>
+                                <TabPanel>
+                                    <h2>Any content 2</h2>
+                                </TabPanel>
+                            </div>
+                        </Tabs>
+
+                        <div className='mt-20 flex gap-10'>
+                            <button onClick={handlePrev}>prev</button>
+                            <button onClick={handleNext}>next</button>
+                        </div>
+                    </div>
                 </div>
             </section>
         </main>
