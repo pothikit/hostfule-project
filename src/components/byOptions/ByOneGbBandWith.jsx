94398bd697709e1669074206f000b8b91c9b1aae
pothikit <tytanhsot71@gmail.com>
2024-02-11 13:28:51 +0600

    display data loading by 100

--- src/components/byOptions/ByOneGbBandWith.jsx
+++ src/components/byOptions/ByOneGbBandWith.jsx
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from 'react'
 import DetailsbyOptions from '../dedicatedServer/locationDatatable/DetailsbyOptions'
 import Loading from './../shared/loading/Loading';
 
-
 const ByOneGbBandWith = () => {
     const [country, setCountry] = useState([])
     const [loading, setLoadin] = useState(false)
+    const [displayData, setDisplayData] = useState(100)
     useEffect(() => {
         fetch("../locationdata.json")
             .then(res => res.json())
@@ -14,7 +14,6 @@ const ByOneGbBandWith = () => {
                 setLoadin(true)
             })
     }, [])
-
     let cities = []
     for (let i = 0; i <= country?.length; i++) {
         country[i]?.city?.map(items => cities.push(items))
@@ -24,7 +23,10 @@ const ByOneGbBandWith = () => {
         cities[i]?.details?.map(details => detail.push(details))
     }
     const ongbpsSpeed = detail?.filter(items => (items.networkSpeed === "1 Gbps"))
-    // console.log(ongbpsSpeed)
+
+    const handleDisplayData = () => {
+        setDisplayData(displayData + 100)
+    }
     if (!loading) {
         return <div className='py-20'><Loading></Loading></div>
     }
@@ -34,9 +36,18 @@ const ByOneGbBandWith = () => {
                 <div className="container mx-auto">
                     <div className=''>
                         {
-                            ongbpsSpeed?.map((speedDataTabel, idx) => (
-                                <DetailsbyOptions loading={loading} key={idx} tableData={speedDataTabel}></DetailsbyOptions>
+                            ongbpsSpeed?.slice(0, displayData).map((speedDataTabel, idx) => (
+                                <>
+                                    <DetailsbyOptions loading={loading} key={idx} tableData={speedDataTabel}></DetailsbyOptions>
+                                    <div>{idx + 1}</div>
+                                </>
                             ))
+                        }
+
+                    </div>
+                    <div className='text-center my-10'>
+                        {
+                            displayData < ongbpsSpeed.length && <button className='py-2 px-4 hover:bg-opacity-80 rounded-lg md:px-6 bg-primary rounded-mg text-white' onClick={handleDisplayData}>Load more</button>
                         }
                     </div>
                 </div>
