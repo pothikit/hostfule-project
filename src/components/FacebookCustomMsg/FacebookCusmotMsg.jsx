c4b3b7777ad2808e709b4f0a8018c72b821aab1d
pothikit <tytanhsot71@gmail.com>
2024-02-22 15:57:35 +0600

    ref noreferre seted

--- src/components/FacebookCustomMsg/FacebookCusmotMsg.jsx
+++ src/components/FacebookCustomMsg/FacebookCusmotMsg.jsx
@@ -4,9 +4,7 @@ import { FacebookProvider, CustomChat } from 'react-facebook';
 export default class FacebookCustomMsg extends Component {
     render() {
         return (
-            // page id 198137493375467
-            //app id 782029750618062
-            <FacebookProvider appId="782029750618062" chatSupport>
+            <FacebookProvider appId="782029750618062" chatSupport rel="noreferrer">
                 <CustomChat pageId="198137493375467" minimized="false" />
             </FacebookProvider>
         );
